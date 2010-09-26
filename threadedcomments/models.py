7c1b8dc0204ddf8b202bd7d8b9d024dd8dc058de
Craig de Stigter <craig.destigter@koordinates.com>
2010-09-26 20:01:42 +1300

    make django-threadedcomments compatible with the upcoming django-mptt 0.4

--- threadedcomments/models.py
+++ threadedcomments/models.py
@@ -5,10 +5,17 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 import mptt
 
+try:
+    from mptt.models import MPTTModel
+except ImportError:
+    # mptt < 0.4
+    MPTTModel = object
+
+__all__ = ('MAX_PATH_LENGTH', 'ThreadedComment')
+
 MAX_PATH_LENGTH = 255
 
-
-class ThreadedComment(Comment):
+class ThreadedComment(Comment, MPTTModel):
     title = models.TextField(_('Title'), blank=True)
     parent = models.ForeignKey('self', null=True, blank=True, default=None,
         related_name='children', verbose_name=_('Parent'))
@@ -20,5 +27,6 @@ class ThreadedComment(Comment):
         verbose_name = _('Threaded comment')
         verbose_name_plural = _('Threaded comments')
 
-mptt.register(ThreadedComment)
-
+if hasattr(mptt, 'register'):
+    # mptt < 0.4
+    mptt.register(ThreadedComment)
