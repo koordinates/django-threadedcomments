c5e8442ed2367bc58f7b67697192b6a9a7198474
Craig de Stigter <craig.destigter@koordinates.com>
2011-02-21 16:17:13 +1300

    cleanup. make threadedcomments work only with 0.4.2+

--- threadedcomments/models.py
+++ threadedcomments/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 from django.contrib.comments.models import Comment
 from django.contrib.comments.managers import CommentManager
-from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-import mptt
 
-try:
-    from mptt.models import MPTTModel
-except ImportError:
-    # mptt < 0.4
-    MPTTModel = object
+from mptt.models import MPTTModel
 
 __all__ = ('MAX_PATH_LENGTH', 'ThreadedComment')
 
@@ -26,7 +20,3 @@ class ThreadedComment(Comment, MPTTModel):
         db_table = 'threadedcomments_comment'
         verbose_name = _('Threaded comment')
         verbose_name_plural = _('Threaded comments')
-
-if hasattr(mptt, 'register'):
-    # mptt < 0.4
-    mptt.register(ThreadedComment)
